81f3d043c78cdcef33b7330cb655e53b7ae818ee
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-03 15:43:55 +0200

    ADD: form to submit paragraph and word in server.php file through POST method + server.php file with printed original paragraph, its length and new paragraph with censored word and its length

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <main>
+        <form action="./server.php" method="POST">
+            <p>
+                <label for="paragraph">Testo:</label><br>
+                <textarea name="paragraph" id="paragraph" placeholder="Il tuo testo" rows="10" cols="30"></textarea>
+            </p>
+            <p>
+                <label for="word">Parola da censurare:</label><br>
+                <input type="text" name="word" id="word"/>
+            </p>
+            <p>
+                <button type="submit">Invia</button>
+            </p>
+        </form>
+    </main>
 </body>
 </html>
---
+++ server.php
@@ -0,0 +1,35 @@
+<?php
+
+//var_dump($_POST);
+
+$paragraph = $_POST['paragraph'];
+$word = $_POST['word'];
+$paragraph_length = strlen($paragraph);
+
+$new_paragraph = str_replace($word, '***', $paragraph);
+$new_paragraph_length = strlen($new_paragraph);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords Result</title>
+</head>
+<body>
+    <main>
+        <h5>Paragrafo originale:</h5>
+        <?php echo $paragraph ?>
+        <h5>Lunghezza iniziale paragrafo:</h5>
+        <?php echo $paragraph_length ?>
+
+        <h5>Paragrafo censurato:</h5>
+        <?php echo $new_paragraph ?>
+        <h5>Nuova lunghezza paragrafo:</h5>
+        <?php echo $new_paragraph_length ?>
+    </main>
+</body>
+</html>
